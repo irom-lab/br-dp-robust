940b73ce1c8a558597e3f064074b4c3b34074323
Vincent Pacelli <vpacelli@princeton.edu>
2021-06-20 23:01:02 -0400

    Altered imports for new controller structure.

--- rationality/controllers/__init__.py
+++ rationality/controllers/__init__.py
@@ -1,5 +1,7 @@
 import rationality.controllers.util
 
-from rationality.controllers.lqr import lqr, cost_to_go as lqr_cost_to_go
+from rationality.controllers.lqr import lqr, cost_to_go as lqr_cost_to_go, LQRParams
+from rationality.controllers.isc import isc, ISCParams
+
 from rationality.controllers.types import *
 
