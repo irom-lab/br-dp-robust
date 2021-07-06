ca40f845c4e9495e23f90dec17f1765001d32be7
Vincent Pacelli <vpacelli@princeton.edu>
2021-07-06 15:37:04 -0400

    Renamed for consistency. Added `ordinal` function.

--- rationality/util.py
+++ rationality/util.py
@@ -9,3 +9,7 @@ def in_ipynb():
             return False  # Other type (?)
     except NameError:
         return False
+
+
+def ordinal(n: int) -> str:
+    return "%d%s" % (n, 'tsnrhtdd' [(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10::4])
