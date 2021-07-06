308631a9c78dfba969f99919f6ff0e0f022badb1
Vincent Pacelli <vpacelli@princeton.edu>
2021-07-06 16:36:10 -0400

    Fixed PEP8 warning.

--- rationality/controllers/__init__.py
+++ rationality/controllers/__init__.py
@@ -1,6 +1,5 @@
 import rationality.controllers.util
 
-from rationality.controllers import lqr, isc, svmpc
+from rationality.controllers import lqr, isc, svmpc, mpc
 
 from rationality.controllers.types import *
-
