2c2cf7b9733488a88599b861252736787a7f32ee
Vincent Pacelli <vpacelli@princeton.edu>
2021-08-19 02:19:56 -0400

    Added `lqbr`.

--- rationality/controllers/__init__.py
+++ rationality/controllers/__init__.py
@@ -1,5 +1,5 @@
 import rationality.controllers.util
 
-from rationality.controllers import lqr, isc, svmpc, mpc
+from rationality.controllers import lqr, isc, svmpc, mpc, lqbr
 
 from rationality.controllers.types import *
