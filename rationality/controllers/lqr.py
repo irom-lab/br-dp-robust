3e6035c73e54a933a6cd7d264d24cf91aeebfe67
Vincent Pacelli <vpacelli@princeton.edu>
2021-08-19 02:19:31 -0400

    Flipped sign convention on K.

--- rationality/controllers/lqr.py
+++ rationality/controllers/lqr.py
@@ -17,8 +17,8 @@ def create(prob: Problem) -> Controller:
 @jax.jit
 def lqr_scanner(P: jnp.ndarray, _, A: jnp.ndarray, B: jnp.ndarray,
                 Q: jnp.ndarray, R: jnp.ndarray) -> tuple[jnp.ndarray, jnp.ndarray]:
-    K = jnp.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
-    P = Q + A.T @ P @ A - A.T @ P @ B @ K
+    K = -jnp.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
+    P = Q + A.T @ P @ A + A.T @ P @ B @ K
 
     return P, K
 
@@ -26,7 +26,7 @@ def lqr_scanner(P: jnp.ndarray, _, A: jnp.ndarray, B: jnp.ndarray,
 def lqr_dynamic_programming(prob_params: ProblemParams, horizon: int) -> tuple[jnp.ndarray, jnp.ndarray]:
     dynamics, objective = prob_params
     A, B = dynamics
-    Q, R, Qf = objective
+    Q, R, Qf, _, _ = objective
     init = Qf
 
     P, K = jax.lax.scan(lambda c, _: lqr_scanner(c, _, A, B, Q, R), init, None, length=horizon, reverse=True)
@@ -46,7 +46,7 @@ def lqr_prototype(state: State, t: int, controller_state: LQRControllerState,
                   temporal_info: LQRTemporalInfo, params: LQRParams) -> tuple[Input, LQRControllerState]:
     K = temporal_info
 
-    return -K @ state, None
+    return K @ state, None
 
 
 def cost_to_go(state: State, t: int, prob: Problem) -> float:
