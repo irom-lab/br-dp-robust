4e93c4b528e92d32a731d3a02cdb1709a52e6e06
Vincent Pacelli <vpacelli@princeton.edu>
2021-09-07 17:55:09 -0400

    Added `util.savefig`

--- rationality/util.py
+++ rationality/util.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import os
+
+from typing import Iterable, Optional
+
 def in_ipynb():
     try:
         shell = get_ipython().__class__.__name__
@@ -13,3 +18,19 @@ def in_ipynb():
 
 def ordinal(n: int) -> str:
     return "%d%s" % (n, 'tsnrhtdd' [(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10::4])
+
+
+def savefig(base_name: str, formats: Iterable[str]) -> None:
+    for format in formats:
+        filename = f'{base_name}.{format}'
+        print(f'Saving {format.upper()} format: {filename}')
+
+        if os.path.exists(filename):
+            os.remove(filename)
+
+        if format in {'pgf'}:
+            plt.savefig(f'{base_name}.{format}', format=format, bbox_inches='tight', pad_inches=0.0, backend='pgf')
+        elif format in {'eps', 'jpeg', 'jpg', 'pdf', 'pgf', 'png', 'ps', 'raw', 'rgba', 'svg', 'svgz', 'tif', 'tiff'}:
+            plt.savefig(f'{base_name}.{format}', format=format, bbox_inches='tight', pad_inches=0.0)
+        else:
+            raise ValueError(f"Unknown file format: `{format}'.")
