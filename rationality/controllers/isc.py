756f688c548d873825a846be644f85ff1dfc0308
Vincent Pacelli <vpacelli@princeton.edu>
2021-10-14 15:44:47 -0400

    Documented isc.py

--- rationality/controllers/isc.py
+++ rationality/controllers/isc.py
@@ -1,3 +1,7 @@
+"""
+Importance Sampling Controller (ISC).
+"""
+
 from functools import partial
 
 import jax
@@ -9,24 +13,46 @@ import rationality.distributions as dst
 import rationality.inference as inf
 from rationality.controllers.types import *
 
+from typing import Iterable, Union
+
 ISTemporalInfo = Any
 ISControllerState = jnp.ndarray
 
 
 class ISCParams(NamedTuple):
+    """
+    Parameters for the ISC
+
+    inv_temp: The inverse temperature (beta) value of the controller.
+    """
     inv_temp: float
-    init_key: jnp.ndarray
 
 
-def create(prob: Problem, inv_temp: float, num_samples: int, init_key: jnp.ndarray,
-           prior_proto: dst.DistributionPrototype, prior_params: list[tuple]) -> Controller:
-    num_prior_params = len(prior_params[0])
-    prior_params_for_scanning = tuple(jnp.stack([p[i] for p in prior_params]) for i in range(num_prior_params))
-    params = ISCParams(inv_temp, init_key)
+def create(prob: Problem, inv_temp: float, num_samples: int,
+           prior_proto: dst.DistributionPrototype,
+           prior_params: Union[Iterable[dst.DistributionParams], dst.DistributionParams]) -> Controller:
+    """
+    Creates a Controller instance using the ISC prototype functions and parameters.
+
+    :param prob: The control problem instance.
+    :param inv_temp: The inverse temperature value (beta) to use for the controller.
+    :param num_samples: The number of samples used in the importance sampling procedure.
+    :param prior_proto: The prototype describing the prior distribution.
+    :param prior_params: The sequence of parameters describing the prior distribution at each time step. These may
+                         either be specified as a list of parameter object instances or a single instance where
+                         the first axis of each parameter is the time index.
+
+    :returns: The controller instance.
+    """
+    if not dst.isdistparams(prior_params):
+        num_prior_params = len(prior_params[0])
+        prior_params = tuple(jnp.stack([p[i] for p in prior_params]) for i in range(num_prior_params))
+
+    params = ISCParams(inv_temp)
     cost_of_ctl_seq = util.compile_cost_of_control_sequence(prob)
 
-    init_isc = jax.jit(lambda prob_params, isc_params, key: init_isc_prototype(prob_params, isc_params, key,
-                                                                          prior_params_for_scanning))
+    init_isc = jax.jit(lambda prob_params, isc_params, key: init_isc_prototype(prob_params, isc_params,
+                                                                               key, prior_params))
     isc_controller = jax.jit(lambda state, t, controller_state, temporal_info, params:
                              isc_prototype(state, t, controller_state, temporal_info, params,
                                            prior_proto, num_samples, prob.prototype, cost_of_ctl_seq))
@@ -36,22 +62,40 @@ def create(prob: Problem, inv_temp: float, num_samples: int, init_key: jnp.ndarr
 
 def init_isc_prototype(params: ProblemParams, isc_params: ISCParams, key: jnp.ndarray,
                        prior_params: Any) -> tuple[ISControllerState, ISTemporalInfo]:
+    """
+    Initialize the ISC by returning the controller state and temporal information.
+
+    :param params: Unused
+    :param isc_params: Unused
+    :param key: The RNG key that will be used for the simulation.
+    :param prior_params: The parameters describing the prior distribution. Each attribute of the object must be at least
+                         2-dimensional with the first axis indexing the temporal sequence of parameters.
+
+    :returns: The tuple of state and temporal information for the ISC.
+    """
     return key, prior_params
-
-
-def hamiltonian(state: State, input_seq: Input, t: int, proto: ProblemPrototype,
-                cost_of_ctl_seq: Callable[[State, int, Input], float]) -> float:
-    inputs = jnp.pad(input_seq.reshape((proto.dynamics.num_inputs, -1), order='F'),
-                     [(0, 0), (0, 1)])
-
-    return cost_of_ctl_seq(state, t, inputs)
 
 
 @partial(jax.jit, static_argnums=(5, 6, 7, 8))
 def isc_prototype(state: State, t: int, controller_state: ISControllerState,
-                  temporal_info: Any, params: ISCParams,
+                  temporal_info: ISTemporalInfo, params: ISCParams,
                   prior_proto: dst.DistributionPrototype, num_samples: int, prob_proto: ProblemPrototype,
                   cost_of_ctl_seq: Callable[[State, int, Input], float]) -> tuple[Input, ISControllerState]:
+    """
+    The prototype for the ISC controller.
+
+    :param state: The current system state.
+    :param t: The current time step.
+    :param controller_state: The current state of the controller.
+    :param temporal_info: The state-independent temporal information provided to the controller at the current time.
+    :param num_samples: The number of samples used for importance sampling.
+    :param prob_proto: The prototype of the control problem.
+    :param cost_of_ctl_seq: A function that maps (x, t, u) to a cost, where x is the current state (n-dimensional
+                            vector), t is the current time step, and u (m-by-t_f array where t_f is the problem
+                            horizon), is the sequence of control inputs.
+
+    :returns: A tuple (u, isc_state) containing the control input and the ISC State respectively.
+    """
     key, subkey = rnd.split(controller_state, 2)
     input_samples = prior_proto.sample(num_samples, subkey, temporal_info)
 
@@ -60,7 +104,11 @@ def isc_prototype(state: State, t: int, controller_state: ISControllerState,
 
     @jax.jit
     def zero_temp_case(input_sequences: jnp.ndarray) -> jnp.ndarray:
-        costs = jax.vmap(lambda u: hamiltonian(state, u, t, prob_proto, cost_of_ctl_seq), in_axes=1)(input_sequences)
+        """
+        This function handles the limiting case where inv_temp is infinite. In this case, we simply take the best
+        performing input sequence.
+        """
+        costs = jax.vmap(lambda u: util.hamiltonian(state, u, t, prob_proto, cost_of_ctl_seq), in_axes=1)(input_sequences)
 
         return jnp.take(input_sequences, jnp.argmin(costs), axis=1)
 
